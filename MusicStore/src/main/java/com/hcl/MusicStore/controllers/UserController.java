2730d03b63a546a10d1ac3e7ad1e69af845acb50
Luc <ludan1214@gmail.com>
2021-04-05 23:08:15 -0500

    hotfix

--- MusicStore/src/main/java/com/hcl/MusicStore/controllers/UserController.java
+++ MusicStore/src/main/java/com/hcl/MusicStore/controllers/UserController.java
@@ -66,7 +66,7 @@ public class UserController {
     		if (user == null) {
     			throw new UserNotFoundException(username);
     		} else {
-    			//
+    			// 
     		}
     	return "catalog";
     }
