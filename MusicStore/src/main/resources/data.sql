27cf34c1d83b0dbf446423c2dfc1c2660dd83acc
Matt Stallman <matthew.stallman@hcl.com>
2021-04-06 10:38:10 -0500

    sql syntax change in data.sql file

--- MusicStore/src/main/resources/data.sql
+++ MusicStore/src/main/resources/data.sql
@@ -2,6 +2,15 @@ insert into product_tbl
 	(title, artist, style, format, price, genre, quantity)
 values
 	('Testing1', 'Tester', 'folk', 'digital', 15, 'rock', 3);
+insert into product_tbl
+	(title, artist, style, format, price, genre, quantity)
+values
 	('Testing2', 'Tester', 'folk', 'digital', 15, 'rock', 3);
+insert into product_tbl
+	(title, artist, style, format, price, genre, quantity)
+values
 	('Testing3', 'Tester', 'symphony', 'cd', 15, 'rock', 3);
+insert into product_tbl
+	(title, artist, style, format, price, genre, quantity)
+values
 	('Does it Really Work?', 'Joe Mama', 'hard core', 'vinyl', 15, 'electronic', 3);
