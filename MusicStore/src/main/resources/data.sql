779de235bfa659a0ded7ac765f09d8b1dc9c0176
henryzhu000 <78283465+henryzhu000@users.noreply.github.com>
2021-04-08 14:26:57 -0700

    something wrong with project structure

--- MusicStore/src/main/resources/data.sql
+++ MusicStore/src/main/resources/data.sql
@@ -1,22 +1,18 @@
 truncate product_tbl;
-alter table product_tbl modify price decimal(5,2);
-
 
 insert into product_tbl
-	(id,category,imageurl,title, artist, style, format, price, genre, quantity)
+	(product_id,title, artist, style, format, price, genre, quantity)
 values
-	(0,'Electronics','images/turntable1-1280.jpg','Airpods',"N/A", 'folk', 'digital', 15.00, 'rock', 3);
-
+	(0,'Testing1', 'Tester', 'folk', 'digital', 15, 'rock', 3);
 insert into product_tbl
-	(category,imageurl,title, artist, style, format, price, genre, quantity)
+	(product_id,title, artist, style, format, price, genre, quantity)
 values
-	('Electronics','images/turntable1-1280.jpg','Airpods',"N/A", 'folk', 'digital', 15.00, 'rock', 3);
+	(0,'Testing2', 'Tester', 'folk', 'digital', 15, 'rock', 3);
 insert into product_tbl
-	(category,imageurl,title, artist, style, format, price, genre, quantity)
+	(product_id,title, artist, style, format, price, genre, quantity)
 values
-	('Electronics','images/turntable1-1280.jpg','Airpods',"N/A", 'folk', 'digital', 15.00, 'rock', 3);
+	(0,'Testing3', 'Tester', 'symphony', 'cd', 15, 'rock', 3);
 insert into product_tbl
-	(category,imageurl,title, artist, style, format, price, genre, quantity)
+	(product_id,title, artist, style, format, price, genre, quantity)
 values
-	('Electronics','images/turntable1-1280.jpg','Airpods',"N/A", 'folk', 'digital', 15.00, 'rock', 3);
-
+	(0,'Does it Really Work?', 'Joe Mama', 'hard core', 'vinyl', 15, 'electronic', 3);
