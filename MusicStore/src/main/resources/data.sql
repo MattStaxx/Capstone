9ffba4c39a40efbb541b3fcd00a0064409a98b9d
nehemiah.sam <nehemiah.sam@LP-PF2J02D1.GEO.CORP.HCL.IN>
2021-04-08 11:29:48 -0500

    SQL changes

--- MusicStore/src/main/resources/data.sql
+++ MusicStore/src/main/resources/data.sql
@@ -1,18 +1,22 @@
 truncate product_tbl;
 
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(id,category,title, artist, style, format, price, genre, quantity)
 values
-	(0,'Testing1', 'Tester', 'folk', 'digital', 15, 'rock', 3);
+	(0,'Music','Testing1', 'Tester', 'folk', 'digital', 15, 'rock', 3);
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(id,category,title, artist, style, format, price, genre, quantity)
 values
-	(0,'Testing2', 'Tester', 'folk', 'digital', 15, 'rock', 3);
+	(0,'Music','Testing2', 'Tester', 'folk', 'digital', 15, 'rock', 3);
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(id,category,title, artist, style, format, price, genre, quantity)
 values
-	(0,'Testing3', 'Tester', 'symphony', 'cd', 15, 'rock', 3);
+	(0,'Music','Testing3', 'Tester', 'symphony', 'cd', 15, 'rock', 3);
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(id,category,title, artist, style, format, price, genre, quantity)
 values
-	(0,'Does it Really Work?', 'Joe Mama', 'hard core', 'vinyl', 15, 'electronic', 3);
+	(0,'Music','Does it Really Work?', 'Joe Mama', 'hard core', 'vinyl', 15, 'electronic', 3);
+insert into product_tbl
+	(id,category,title, artist, style, format, price, genre, quantity)
+values
+	(0,'Electronics','Airpods',"N/A", 'folk', 'digital', 15, 'rock', 3);
