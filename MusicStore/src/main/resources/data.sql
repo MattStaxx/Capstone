88530f5ef1b3485f0a5aa43b6bc4e7e866106b48
henry333666999 <35183729+henry333666999@users.noreply.github.com>
2021-04-08 18:22:47 -0400

    Update data.sql
    product_id doesn't exist

--- MusicStore/src/main/resources/data.sql
+++ MusicStore/src/main/resources/data.sql
@@ -1,18 +1,18 @@
 truncate product_tbl;
 
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(id,title, artist, style, format, price, genre, quantity)
 values
 	(0,'Testing1', 'Tester', 'folk', 'digital', 15, 'rock', 3);
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(id,title, artist, style, format, price, genre, quantity)
 values
 	(0,'Testing2', 'Tester', 'folk', 'digital', 15, 'rock', 3);
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(id,title, artist, style, format, price, genre, quantity)
 values
 	(0,'Testing3', 'Tester', 'symphony', 'cd', 15, 'rock', 3);
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(id,title, artist, style, format, price, genre, quantity)
 values
 	(0,'Does it Really Work?', 'Joe Mama', 'hard core', 'vinyl', 15, 'electronic', 3);
