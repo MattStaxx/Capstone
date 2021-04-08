760ed1397328cb0339b3f346194650235787e2b4
henry333666999 <35183729+henry333666999@users.noreply.github.com>
2021-04-08 11:47:26 -0400

    Update data.sql

--- MusicStore/src/main/resources/data.sql
+++ MusicStore/src/main/resources/data.sql
@@ -1,18 +1,18 @@
 truncate product_tbl;
 
 insert into product_tbl
-	(title, artist, style, format, price, genre, quantity)
+	(product_id,title, artist, style, format, price, genre, quantity)
 values
-	('Testing1', 'Tester', 'folk', 'digital', 15, 'rock', 3);
+	(0,'Testing1', 'Tester', 'folk', 'digital', 15, 'rock', 3);
 insert into product_tbl
-	(title, artist, style, format, price, genre, quantity)
+	(product_id,title, artist, style, format, price, genre, quantity)
 values
-	('Testing2', 'Tester', 'folk', 'digital', 15, 'rock', 3);
+	(0,'Testing2', 'Tester', 'folk', 'digital', 15, 'rock', 3);
 insert into product_tbl
-	(title, artist, style, format, price, genre, quantity)
+	(product_id,title, artist, style, format, price, genre, quantity)
 values
-	('Testing3', 'Tester', 'symphony', 'cd', 15, 'rock', 3);
+	(0,'Testing3', 'Tester', 'symphony', 'cd', 15, 'rock', 3);
 insert into product_tbl
-	(title, artist, style, format, price, genre, quantity)
+	(product_id,title, artist, style, format, price, genre, quantity)
 values
-	('Does it Really Work?', 'Joe Mama', 'hard core', 'vinyl', 15, 'electronic', 3);
+	(0,'Does it Really Work?', 'Joe Mama', 'hard core', 'vinyl', 15, 'electronic', 3);
