39d65844e3a66e71a46702758ea2712c5350c98a
Luc <ludan1214@gmail.com>
2021-04-08 17:25:22 -0500

    Database

--- MusicStore/src/main/resources/data.sql
+++ MusicStore/src/main/resources/data.sql
@@ -1,18 +1,18 @@
 truncate product_tbl;
 
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(title, artist, style, format, price, genre, quantity)
 values
-	(0,'Testing1', 'Tester', 'folk', 'digital', 15, 'rock', 3);
+	('Testing1', 'Tester', 'folk', 'digital', 15, 'rock', 3);
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(title, artist, style, format, price, genre, quantity)
 values
-	(0,'Testing2', 'Tester', 'folk', 'digital', 15, 'rock', 3);
+	('Testing2', 'Tester', 'folk', 'digital', 15, 'rock', 3);
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(title, artist, style, format, price, genre, quantity)
 values
-	(0,'Testing3', 'Tester', 'symphony', 'cd', 15, 'rock', 3);
+	('Testing3', 'Tester', 'symphony', 'cd', 15, 'rock', 3);
 insert into product_tbl
-	(product_id,title, artist, style, format, price, genre, quantity)
+	(title, artist, style, format, price, genre, quantity)
 values
-	(0,'Does it Really Work?', 'Joe Mama', 'hard core', 'vinyl', 15, 'electronic', 3);
+	('Does it Really Work?', 'Joe Mama', 'hard core', 'vinyl', 15, 'electronic', 3);
