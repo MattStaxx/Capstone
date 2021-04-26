7c51071c8dcad4438fcd39e8ad12f0f2330207f6
Luc <ludan1214@gmail.com>
2021-04-26 13:30:04 -0500

    display catalog uses a parallel stream

--- MusicStore/src/main/java/com/hcl/MusicStore/services/ProductService.java
+++ MusicStore/src/main/java/com/hcl/MusicStore/services/ProductService.java
@@ -3,6 +3,7 @@ package com.hcl.MusicStore.services;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -88,37 +89,12 @@ public class ProductService {
 		return query.getResultList();
 	}
 
-	public List<Product> searchForProducts2(String selection, String name, String sortName, int asc, int startingPrice, int endingPrice) {
-		logger.info("In Product Service... " + selection + " " + name);
-		CriteriaBuilder finding= em.getCriteriaBuilder();
-		CriteriaQuery<Product>  result= finding.createQuery(Product.class);
-		
-	    Root<Product> searchSet= result.from(Product.class);
-	    System.out.println("asc is: "+asc+" "+sortName);
-	    if(name.length()>0) {
-		Predicate searchPredicate= finding.equal(searchSet.get(selection), name);
-		result.where(searchPredicate);
-	    }
-	     if(asc==1){
-	    	result.orderBy(finding.asc(searchSet.get(sortName)));
-	    }
-	    else if(asc==2){
-	    	result.orderBy(finding.desc(searchSet.get(sortName)));
-	    }
-		
-		TypedQuery<Product> query = em.createQuery(result);
-		
-		return query.getResultList();
-	}
 
 	public List<Product> displayCatalog(){
-        List<Product> products = productRepository.findAll();
-        List<Product> catalog = new ArrayList<Product>();
-        for (Product p : products) {
-            if (p.getCustomerOrder() == null && p.getCustomer() == null) {
-                catalog.add(p);
-            }
-        }
+		List<Product> products = productRepository.findAll();
+        List<Product> catalog = products.parallelStream()
+        		.filter(p -> p.getCustomerOrder() == null && p.getCustomer() == null)
+        		.collect(Collectors.toList());
         return catalog;
     }   
 }
